2fd67720b03bbb16d24dfdbc3243986c460dd826
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-07-31 23:33:23 +0530

    feat: initialize with basic server setup

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser()); 
+
+app.use(
+  cors({
+    origin: process.env.BASE_URL,
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: ['Content-Type', "Authorization"]
+  })
+);
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send("Hello world");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
